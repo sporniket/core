df497c7dbc03ef21e3fbb3030bc215de99bc4312
David SPORN <sporniket.studio@gmail.com>
2015-03-05 17:51:35 +0100

    cleanstep-javadoc : add the GNU LGPL notice

--- sporniket-core-ui/src/main/java/com/sporniket/libre/ui/icon/IconProviderException.java
+++ sporniket-core-ui/src/main/java/com/sporniket/libre/ui/icon/IconProviderException.java
@@ -5,6 +5,30 @@ package com.sporniket.libre.ui.icon;
 
 /**
  * Exception thrown when an Icon provider has a problem.
+ * <p>
+ * &copy; Copyright 2002-2015 David Sporn
+ * </p>
+ * <hr>
+ * 
+ * <p>
+ * This file is part of <i>The Sporniket Core Library &#8211; ui</i>.
+ * 
+ * <p>
+ * <i>The Sporniket Core Library &#8211; ui</i> is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ * 
+ * <p>
+ * <i>The Sporniket Core Library &#8211; ui</i> is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
+ * more details.
+ * 
+ * <p>
+ * You should have received a copy of the GNU Lesser General Public License along with <i>The Sporniket Core Library &#8211; ui</i>.
+ * If not, see <a href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>. 2
+ * 
+ * <hr>
+ * 
  * @author David SPORN 
  *
  * @version 15.02.00
