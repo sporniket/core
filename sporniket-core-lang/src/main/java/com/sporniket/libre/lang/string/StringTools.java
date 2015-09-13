5bdbf2d6353b8d794ea7a406c42f76cbdee93e32
David SPORN <sporniket.studio@gmail.com>
2015-09-13 14:28:20 +0200

    fix #16 - remove space at the beginning and/or at the end of a given String

--- sporniket-core-lang/src/main/java/com/sporniket/libre/lang/string/StringTools.java
+++ sporniket-core-lang/src/main/java/com/sporniket/libre/lang/string/StringTools.java
@@ -34,12 +34,50 @@ import com.sporniket.libre.lang.DataTools;
  * 
  * <hr>
  * 
- * @author David SPORN 
+ * @author David SPORN
  * @version 15.02.00
  * @since 12.06.01
  */
 public class StringTools
 {
+	/**
+	 * Mode for {@link StringTools#removeWhiteSpaces(String, SpaceRemovingMode)}.
+	 * 
+	 * <p>
+	 * &copy; Copyright 2002-2015 David Sporn
+	 * </p>
+	 * <hr>
+	 * 
+	 * <p>
+	 * This file is part of <i>The Sporniket Core Library &#8211; lang</i>.
+	 * 
+	 * <p>
+	 * <i>The Sporniket Core Library &#8211; lang</i> is free software: you can redistribute it and/or modify it under the terms of
+	 * the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at
+	 * your option) any later version.
+	 * 
+	 * <p>
+	 * <i>The Sporniket Core Library &#8211; lang</i> is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+	 * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+	 * License for more details.
+	 * 
+	 * <p>
+	 * You should have received a copy of the GNU Lesser General Public License along with <i>The Sporniket Core Library &#8211;
+	 * lang</i>. If not, see <a href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>. 2
+	 * 
+	 * <hr>
+	 * 
+	 * @author David SPORN
+	 * @version 15.09.00
+	 * @since 15.09.00
+	 */
+	public static enum SpaceRemovingMode
+	{
+		LEADING_SPACES,
+		TRAILING_SPACES,
+		TWO_ENDS_SPACES
+	}
+
 	private static final String explode__DEFAULT_PATTERN = "\\s";
 
 	private static final String[] explode__EMPTY_ARRAY = new String[0];
@@ -197,6 +235,67 @@ public class StringTools
 		{
 			throw new RuntimeException(_exception);
 		}
+	}
+
+	/**
+	 * Remove whitespaces at the beginning and/or at the end of the given String.
+	 * 
+	 * @param value
+	 *            the String to trim.
+	 * @param mode
+	 *            tells where to remove whitespaces.
+	 * @return a trimmed String
+	 * @since 15.09.00
+	 */
+	public static String removeWhiteSpaces(String value, SpaceRemovingMode mode)
+	{
+		if (null == value)
+		{
+			throw new NullPointerException();
+		}
+		int _resultStart = 0;
+		int _resultEnd = value.length();
+
+		// remove leading spaces
+		if (SpaceRemovingMode.LEADING_SPACES == mode || SpaceRemovingMode.TWO_ENDS_SPACES == mode)
+		{
+			for (int _pos = 0; _pos < _resultEnd; _pos++)
+			{
+				char _currentChar = value.charAt(_pos);
+				if (!removeWhiteSpaces__isWhiteSpace(_currentChar))
+				{
+					break;
+				}
+				_resultStart = _pos + 1;
+			}
+		}
+
+		// remove trailing spaces
+		if (SpaceRemovingMode.TRAILING_SPACES == mode || SpaceRemovingMode.TWO_ENDS_SPACES == mode)
+		{
+			for (int _pos = _resultEnd - 1; _pos >= _resultStart; _pos--)
+			{
+				char _currentChar = value.charAt(_pos);
+				if (!removeWhiteSpaces__isWhiteSpace(_currentChar))
+				{
+					break;
+				}
+				_resultEnd = _pos;
+			}
+		}
+
+		return value.substring(_resultStart, _resultEnd);
+	}
+
+	/**
+	 * @param value
+	 *            char to test.
+	 * @return <code>true</code> if the given char is ' '(space) or tabulation.
+	 * @since 15.09.00
+	 */
+	private static boolean removeWhiteSpaces__isWhiteSpace(char value)
+	{
+		return value == ' ' || value == '\t';
 	}
 
 }
---
+++ sporniket-core-lang/src/test/java/test/sporniket/libre/lang/TestStringTools.java
@@ -0,0 +1,141 @@
+package test.sporniket.libre.lang;
+
+import junit.framework.TestCase;
+
+import com.sporniket.libre.lang.string.StringTools;
+import com.sporniket.libre.lang.string.StringTools.SpaceRemovingMode;
+
+/**
+ * Test suite for {@link StringTools}.
+ * 
+ * <p>
+ * &copy; Copyright 2002-2015 David Sporn
+ * </p>
+ * <hr>
+ * 
+ * <p>
+ * This file is part of <i>The Sporniket Core Library &#8211; lang</i>.
+ * 
+ * <p>
+ * <i>The Sporniket Core Library &#8211; lang</i> is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ * 
+ * <p>
+ * <i>The Sporniket Core Library &#8211; lang</i> is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ * 
+ * <p>
+ * You should have received a copy of the GNU Lesser General Public License along with <i>The Sporniket Core Library &#8211;
+ * lang</i>. If not, see <a href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>. 2
+ * 
+ * <hr>
+ * 
+ * @author David SPORN
+ * @version 15.09.00
+ * @since 15.09.00
+ */
+public class TestStringTools extends TestCase
+{
+	public final void testRemoveHeadingSpaces()
+	{
+		String _valueToTrim = " a";
+		String _valueToExpect = "a";
+		String _valueGot = StringTools.removeWhiteSpaces(_valueToTrim, SpaceRemovingMode.LEADING_SPACES);
+		if (!_valueToExpect.equals(_valueGot))
+		{
+			fail("Some whitespaces has not been detected : expected '" + _valueToExpect + "', got '" + _valueGot + "'.");
+		}
+		_valueToTrim = "  a";
+		_valueGot = StringTools.removeWhiteSpaces(_valueToTrim, SpaceRemovingMode.LEADING_SPACES);
+		if (!_valueToExpect.equals(_valueGot))
+		{
+			fail("Some whitespaces has not been detected : expected '" + _valueToExpect + "', got '" + _valueGot + "'.");
+		}
+		_valueToTrim = "   a";
+		_valueGot = StringTools.removeWhiteSpaces(_valueToTrim, SpaceRemovingMode.LEADING_SPACES);
+		if (!_valueToExpect.equals(_valueGot))
+		{
+			fail("Some whitespaces has not been detected : expected '" + _valueToExpect + "', got '" + _valueGot + "'.");
+		}
+		_valueToExpect = "";
+		_valueToTrim = "   ";
+		_valueGot = StringTools.removeWhiteSpaces(_valueToTrim, SpaceRemovingMode.LEADING_SPACES);
+		if (!_valueToExpect.equals(_valueGot))
+		{
+			fail("Some whitespaces has not been detected : expected '" + _valueToExpect + "', got '" + _valueGot + "'.");
+		}
+	}
+
+	public final void testRemoveSpacesOnBothSides()
+	{
+		String _valueToTrim = " a ";
+		String _valueToExpect = "a";
+		String _valueGot = StringTools.removeWhiteSpaces(_valueToTrim, SpaceRemovingMode.TWO_ENDS_SPACES);
+		if (!_valueToExpect.equals(_valueGot))
+		{
+			fail("Some whitespaces has not been detected : expected '" + _valueToExpect + "', got '" + _valueGot + "'.");
+		}
+		_valueToTrim = "  a ";
+		_valueGot = StringTools.removeWhiteSpaces(_valueToTrim, SpaceRemovingMode.TWO_ENDS_SPACES);
+		if (!_valueToExpect.equals(_valueGot))
+		{
+			fail("Some whitespaces has not been detected : expected '" + _valueToExpect + "', got '" + _valueGot + "'.");
+		}
+		_valueToTrim = "   a  ";
+		_valueGot = StringTools.removeWhiteSpaces(_valueToTrim, SpaceRemovingMode.TWO_ENDS_SPACES);
+		if (!_valueToExpect.equals(_valueGot))
+		{
+			fail("Some whitespaces has not been detected : expected '" + _valueToExpect + "', got '" + _valueGot + "'.");
+		}
+		_valueToExpect = "";
+		_valueToTrim = "   ";
+		_valueGot = StringTools.removeWhiteSpaces(_valueToTrim, SpaceRemovingMode.TWO_ENDS_SPACES);
+		if (!_valueToExpect.equals(_valueGot))
+		{
+			fail("Some whitespaces has not been detected : expected '" + _valueToExpect + "', got '" + _valueGot + "'.");
+		}
+	}
+
+	public final void testRemoveTrailingSpaces()
+	{
+		String _valueToTrim = "a ";
+		String _valueToExpect = "a";
+		String _valueGot = StringTools.removeWhiteSpaces(_valueToTrim, SpaceRemovingMode.TRAILING_SPACES);
+		if (!_valueToExpect.equals(_valueGot))
+		{
+			fail("Some whitespaces has not been detected : expected '" + _valueToExpect + "', got '" + _valueGot + "'.");
+		}
+		_valueToTrim = "a  ";
+		_valueGot = StringTools.removeWhiteSpaces(_valueToTrim, SpaceRemovingMode.TRAILING_SPACES);
+		if (!_valueToExpect.equals(_valueGot))
+		{
+			fail("Some whitespaces has not been detected : expected '" + _valueToExpect + "', got '" + _valueGot + "'.");
+		}
+		_valueToTrim = "a   ";
+		_valueGot = StringTools.removeWhiteSpaces(_valueToTrim, SpaceRemovingMode.TRAILING_SPACES);
+		if (!_valueToExpect.equals(_valueGot))
+		{
+			fail("Some whitespaces has not been detected : expected '" + _valueToExpect + "', got '" + _valueGot + "'.");
+		}
+		_valueToExpect = "";
+		_valueToTrim = "   ";
+		_valueGot = StringTools.removeWhiteSpaces(_valueToTrim, SpaceRemovingMode.TRAILING_SPACES);
+		if (!_valueToExpect.equals(_valueGot))
+		{
+			fail("Some whitespaces has not been detected : expected '" + _valueToExpect + "', got '" + _valueGot + "'.");
+		}
+	}
+
+	public final void testWhiteSpaceCharacterisation()
+	{
+		String _valueToTrim = " \ta";
+		String _valueToExpect = "a";
+		if (!_valueToExpect.equals(StringTools.removeWhiteSpaces(_valueToTrim, SpaceRemovingMode.LEADING_SPACES)))
+		{
+			fail("Some whitespaces has not been detected");
+		}
+	}
+
+}
