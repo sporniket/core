dbc2c2fdc89a9b4e9722297089724ce9140fc69e
David SPORN <sporniket.studio@gmail.com>
2016-08-08 22:13:07 +0200

    cleanstep-javadoc: fixed missing author and licence notice

--- sporniket-core-io/src/main/java/com/sporniket/libre/io/FileComparator.java
+++ sporniket-core-io/src/main/java/com/sporniket/libre/io/FileComparator.java
@@ -8,88 +8,154 @@ import java.util.Comparator;
 
 /**
  * Enclosing type for various comparators.
- * @author dsporn
- *
+ * 
+ * <p>
+ * &copy; Copyright 2002-2015 David Sporn
+ * </p>
+ * <hr>
+ * 
+ * <p>
+ * This file is part of <i>The Sporniket Core Library &#8211; io</i>.
+ * 
+ * <p>
+ * <i>The Sporniket Core Library &#8211; io</i> is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ * 
+ * <p>
+ * <i>The Sporniket Core Library &#8211; io</i> is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
+ * more details.
+ * 
+ * <p>
+ * You should have received a copy of the GNU Lesser General Public License along with <i>The Sporniket Core Library &#8211; io</i>.
+ * If not, see <a href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>. 2
+ * 
+ * <hr>
+ * 
+ * @author David SPORN
  * @version 16.08.00
  * @since 15.02.00
  */
 public class FileComparator
 {
 	/**
-	 * @author dsporn
-	 *
+	 * Comparator by absolute path.
+	 * 
+	 * <p>
+	 * &copy; Copyright 2002-2015 David Sporn
+	 * </p>
+	 * <hr>
+	 * 
+	 * <p>
+	 * This file is part of <i>The Sporniket Core Library &#8211; io</i>.
+	 * 
+	 * <p>
+	 * <i>The Sporniket Core Library &#8211; io</i> is free software: you can redistribute it and/or modify it under the terms of
+	 * the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at
+	 * your option) any later version.
+	 * 
+	 * <p>
+	 * <i>The Sporniket Core Library &#8211; io</i> is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+	 * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+	 * License for more details.
+	 * 
+	 * <p>
+	 * You should have received a copy of the GNU Lesser General Public License along with <i>The Sporniket Core Library &#8211;
+	 * io</i>. If not, see <a href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>. 2
+	 * 
+	 * <hr>
+	 * 
+	 * @author David SPORN
 	 * @version 16.08.00
 	 * @since 15.02.00
 	 */
-	public static class ByAbsolutePath implements Comparator<File> {
+	public static class ByAbsolutePath implements Comparator<File>
+	{
 
-        /**
-         * Comparator to sort by ascending order.
-         */
-        public static final ByAbsolutePath ASCENDING = new ByAbsolutePath();
+		/**
+		 * Comparator to sort by ascending order.
+		 */
+		public static final ByAbsolutePath ASCENDING = new ByAbsolutePath();
 
-        /**
-         * Comparator to sort by descending order.
-         */
-        public static final ByAbsolutePath DESCENDING = new ByAbsolutePath(true);
+		/**
+		 * Comparator to sort by descending order.
+		 */
+		public static final ByAbsolutePath DESCENDING = new ByAbsolutePath(true);
 
-        /**
-         * If <code>true</code>, reverse the order of the comparator.
-         */
-        private boolean myReverse = false;
+		/**
+		 * If <code>true</code>, reverse the order of the comparator.
+		 */
+		private boolean myReverse = false;
 
-        /**
-         * @param reverse
-         */
-        private ByAbsolutePath() {
-            super();
-        }
+		/**
+		 * @param reverse
+		 */
+		private ByAbsolutePath()
+		{
+			super();
+		}
 
-        /**
-         * @param reverse
-         */
-        private ByAbsolutePath(boolean reverse) {
-            setReverse(reverse);
-        }
+		/**
+		 * @param reverse
+		 */
+		private ByAbsolutePath(boolean reverse)
+		{
+			setReverse(reverse);
+		}
 
-        /**
-         * @param o1
-         * @param o2
-         * @return
-         */
-        private int doCompare(File o1, File o2) {
-            if (null == o1 && null == o2) {
-                return 0;
-            } else if (null == o1) {
-                return -1;
-            } else if (null == o2) {
-                return 1;
-            } else {
-                return (o1.getAbsolutePath().compareTo(o2.getAbsolutePath()));
-            }
-        }
+		/**
+		 * @param o1
+		 * @param o2
+		 * @return
+		 */
+		private int doCompare(File o1, File o2)
+		{
+			if (null == o1 && null == o2)
+			{
+				return 0;
+			}
+			else if (null == o1)
+			{
+				return -1;
+			}
+			else if (null == o2)
+			{
+				return 1;
+			}
+			else
+			{
+				return (o1.getAbsolutePath().compareTo(o2.getAbsolutePath()));
+			}
+		}
 
-        /**
-         * @return the reverse
-         */
-        private boolean isReverse() {
-            return myReverse;
-        }
+		/**
+		 * @return the reverse
+		 */
+		private boolean isReverse()
+		{
+			return myReverse;
+		}
 
-        /**
-         * @param reverse
-         *            the reverse to set
-         */
-        private void setReverse(boolean reverse) {
-            myReverse = reverse;
-        }
+		/**
+		 * @param reverse
+		 *            the reverse to set
+		 */
+		private void setReverse(boolean reverse)
+		{
+			myReverse = reverse;
+		}
 
-        public int compare(File o1, File o2) {
-            if (isReverse()) {
-                return -doCompare(o1, o2);
-            } else {
-                return doCompare(o1, o2);
-            }
-        }
-    }
+		public int compare(File o1, File o2)
+		{
+			if (isReverse())
+			{
+				return -doCompare(o1, o2);
+			}
+			else
+			{
+				return doCompare(o1, o2);
+			}
+		}
+	}
 }
--- sporniket-core-ui/src/main/java/com/sporniket/libre/ui/swing/FluidFlowPanelModel.java
+++ sporniket-core-ui/src/main/java/com/sporniket/libre/ui/swing/FluidFlowPanelModel.java
@@ -41,7 +41,7 @@ import javax.swing.JScrollPane;
  * 
  * <hr>
  * 
- * @author David SPORN 
+ * @author David SPORN
  * 
  * @version 16.08.00
  * @since 15.02.00
@@ -49,7 +49,33 @@ import javax.swing.JScrollPane;
 public class FluidFlowPanelModel
 {
 	/**
-	 * @author dsporn
+	 * Listener for updating the panel size when the viewport is resized.
+	 * 
+	 * <p>
+	 * &copy; Copyright 2002-2013 David Sporn
+	 * </p>
+	 * <hr>
+	 * 
+	 * <p>
+	 * This file is part of <i>The Sporniket Core Library &#8211; ui</i>.
+	 * 
+	 * <p>
+	 * <i>The Sporniket Core Library &#8211; ui</i> is free software: you can redistribute it and/or modify it under the terms of
+	 * the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at
+	 * your option) any later version.
+	 * 
+	 * <p>
+	 * <i>The Sporniket Core Library &#8211; ui</i> is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+	 * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+	 * License for more details.
+	 * 
+	 * <p>
+	 * You should have received a copy of the GNU Lesser General Public License along with <i>The Sporniket Core Library &#8211;
+	 * ui</i>. If not, see <a href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>. 2
+	 * 
+	 * <hr>
+	 * 
+	 * @author David SPORN
 	 * 
 	 * @version 16.08.00
 	 * @since 15.02.00
@@ -180,7 +206,8 @@ public class FluidFlowPanelModel
 		JPanel _panel = new JPanel(flowLayout);
 		PanelSizeUpdaterOnViewPortResize _updater = new PanelSizeUpdaterOnViewPortResize(_panel);
 		_panel.addContainerListener(_updater);
-		JScrollPane _scroller = new JScrollPane(_panel,JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		JScrollPane _scroller = new JScrollPane(_panel, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+				JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 		_scroller.addComponentListener(_updater);
 		return new FluidFlowPanelModel(_panel, _scroller);
 	}
