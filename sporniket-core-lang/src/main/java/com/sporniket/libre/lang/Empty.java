646686a5bf3323eaef18f8bd1b6ead2d7d33a64f
SPORN David (Consulting for ACCOR Hotels) <david.sporn@consulting-for.accor.com>
2026-04-16 09:09:45 +0200

    [lang] Empty : change EMPTY_STRING so that it can be marked as covered

--- sporniket-core-lang/src/main/java/com/sporniket/libre/lang/Empty.java
+++ sporniket-core-lang/src/main/java/com/sporniket/libre/lang/Empty.java
@@ -3,31 +3,31 @@ package com.sporniket.libre.lang;
 /**
  * Constants that defines <b>immutable objects</b> that can be seen as <em>empty</em> in some way, they are named to support static
  * imports.
- * 
+ *
  * <p>
  * &copy; Copyright 2002-2022 David Sporn
  * </p>
  * <hr>
- * 
+ *
  * <p>
  * This file is part of <i>The Sporniket Core Library &#8211; lang</i>.
- * 
+ *
  * <p>
  * <i>The Sporniket Core Library &#8211; lang</i> is free software: you can redistribute it and/or modify it under the terms of the
  * GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
  * option) any later version.
- * 
+ *
  * <p>
  * <i>The Sporniket Core Library &#8211; lang</i> is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- * 
+ *
  * <p>
  * You should have received a copy of the GNU Affero General Public License along with <i>The Sporniket Core Library &#8211;
  * lang</i>. If not, see <a href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>. 2
- * 
+ *
  * <hr>
- * 
+ *
  * @author David SPORN
  * @version 22.11.00
  * @since 12.06.01
@@ -37,11 +37,11 @@ public final class Empty
     /**
      * An empty string is a non null String with a length of 0.
      */
-    public static final String EMPTY_STRING = "";
+    public static final String EMPTY_STRING = new String();
 
     /**
      * An empty array is an array of strings with no elements.
-     * 
+     *
      * @since 19.02.00
      */
     public static final String[] EMPTY_STRING_ARRAY = {};
